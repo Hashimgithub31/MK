67299cd9b2aba8545d258eebe69c8384b3247774
Hashimgithub31 <hashim.fshf31@gmail.com>
2021-10-22 11:42:18 +0530

    mohan

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,36 @@
+package org.add;
+
+public class GreensTech {
+	
+	private void greensOmr() {
+		System.out.println("omr greens technology");
+	}
+	
+	private void greensAdayar() {
+		System.out.println("Adayar greens technology");
+	}
+	
+	private void greensTambaram() {
+		System.out.println("Tambaram greens technology");
+	
+	}
+	
+	private void greensVelachery() {
+		System.out.println("Velachery greens technology");
+	}
+	
+	private void greensAnnanagar() {
+		System.out.println("Annanagar greens technology");
+	}
+	
+	public static void main(String[] args) {
+		
+		GreensTech g=new GreensTech();
+		g.greensOmr();
+		g.greensAdayar();
+		g.greensTambaram();
+		g.greensVelachery();
+		g.greensAnnanagar();
+	}
+
+}
